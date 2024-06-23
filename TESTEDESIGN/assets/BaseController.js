acb8d7a41ff4e0cd6ddad1d46dfce5a4d01ece86
G. S. Rocha <123579499+sir-aphim@users.noreply.github.com>
2024-06-23 15:44:28 -0300

    Delete TESTEDESIGN directory

--- TESTEDESIGN/assets/BaseController.js
+++
@@ -1,4 +0,0 @@
-app.controller('BaseController', function($rootScope, $scope, $http, $controller) {
-    angular.extend(this, $controller('FavoritosController', {$rootScope: $rootScope, $scope: $scope, $http: $http}));
-
-});
